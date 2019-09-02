8f914ba5ebfc866cf659fd591fcbe408d9e9e508
wulj13232 <wulj13232@hundsun.com>
2019-09-02 18:41:12 +0800

    spring cloud stream

---
+++ spring-cloud-stream/src/main/java/com/home/spring/cloud/stream/StreamApplication.java
@@ -0,0 +1,13 @@
+package com.home.spring.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author: wulj13232
+ * @description:
+ * @date: Created in 18:38 2019/9/2
+ * @modified by:
+ */
+
+public class StreamApplication {
+}
